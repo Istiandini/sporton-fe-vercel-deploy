79c4a5cfc2e22a5c53f3233008a5c4f5167ae4c0
Istiandini <tiandiniisti@gmail.com>
2026-01-17 16:28:53 +0700

    benerin path gambar products

--- sporton-fe/app/(landing)/components/home/products.tsx
+++ sporton-fe/app/(landing)/components/home/products.tsx
@@ -52,7 +52,7 @@ const ProductsSection = () => {
                 {productList.map((product, index) => (
                     <Link href="#" key={index} className="p-1.5 bg-white hover:drop-shadow-xl duration-300">
                         <div className="bg-primary-light aspect-square w-full flex justify-center items-center relative">
-                            <Image src={`/images/products/${product.imgUrl}`} alt={product.name} width={300} height={300} className="aspect-square object-contain"/>
+                            <Image src={`/images/Products/${product.imgUrl}`} alt={product.name} width={300} height={300} className="aspect-square object-contain"/>
                             <Button className="w-10 h-10 p-2! absolute right-3 top-3"><FiPlus size={24}/></Button>
                         </div>
                         <h3 className="font-medium text-lg mb-1.5 mt-4">{product.name}</h3>
