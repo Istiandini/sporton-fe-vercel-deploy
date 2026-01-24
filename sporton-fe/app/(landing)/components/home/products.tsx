ef35cc1b6992bcd836f864a0dbc02481bc2c1df0
Istiandini <tiandiniisti@gmail.com>
2026-01-24 17:12:21 +0700

    add new code

--- sporton-fe/app/(landing)/components/home/products.tsx
+++ sporton-fe/app/(landing)/components/home/products.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Button from "../ui/button";
 import { FiPlus } from "react-icons/fi";
+import priceFormatter from "@/app/utils/price-formatter";
 
 const productList = [
     {
@@ -44,13 +45,13 @@ const productList = [
 
 const ProductsSection = () => {
     return (
-        <section id="products-section" className="container mx-auto mt-32">
+        <section id="products-section" className="container mx-auto mt-32 mb-52">
             <h2 className="font-bold italic text-4xl text-center mb-11">
                 <span className="text-primary">OUR</span> PRODUCTS
             </h2>
             <div className="grid grid-cols-4">
                 {productList.map((product, index) => (
-                    <Link href="#" key={index} className="p-1.5 bg-white hover:drop-shadow-xl duration-300">
+                    <Link href={`/product/${product.name}`} key={index} className="p-1.5 bg-white hover:drop-shadow-xl duration-300">
                         <div className="bg-primary-light aspect-square w-full flex justify-center items-center relative">
                             <Image src={`/images/Products/${product.imgUrl}`} alt={product.name} width={300} height={300} className="aspect-square object-contain"/>
                             <Button className="w-10 h-10 p-2! absolute right-3 top-3"><FiPlus size={24}/></Button>
@@ -59,11 +60,7 @@ const ProductsSection = () => {
                         <div className="flex justify-between mb-8">
                             <div className="text-gray-500">{product.category}</div>
                             <div className="font-medium text-primary">
-                                {Intl.NumberFormat("id-ID", {
-                                    style: "currency",
-                                    currency: "IDR",
-                                    maximumSignificantDigits: 3,
-                                }).format(product.price)}
+                                {priceFormatter(product.price)}
                             </div>
                         </div>
                     </Link>
