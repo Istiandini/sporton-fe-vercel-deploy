c9b8691d28895d0bee0d41b1b0e578c99fd27ef7
Istiandini <tiandiniisti@gmail.com>
2026-02-14 17:01:36 +0700

    upload new program

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,20 @@
+import { getAllCategories } from "../services/categoriy.service";
+import { getAllProducts } from "../services/product.service";
+import CategoriesSection from "./components/home/categories";
+import HeroSection from "./components/home/hero";
+import ProductsSection from "./components/home/products";
+
+export default async function Home() {
+  const [categories, products] = await Promise.all([
+    getAllCategories(),
+    getAllProducts()
+  ])
+
+  return (
+    <main>
+      <HeroSection/>
+      <CategoriesSection categories={categories} />
+      <ProductsSection products={products} />
+    </main>
+  );
+}
